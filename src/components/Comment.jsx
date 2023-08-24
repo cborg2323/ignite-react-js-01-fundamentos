f910d7cd7abe216a2aa236b143b58ba2343ca5d6
cborg2323 <clobnjuh@gmail.com>
2023-08-24 14:10:10 -0300

    Comment component ok

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img 
+                src="https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FydG9vbiUyMGNoYXJhY3RlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" 
+                alt="Imagem de perfil"
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Clóvis Nunes</strong>
+                            <time title='11 de maio às 08:13h' dateTime='2023-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Diego, parabéns!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
