701f8ba2a6f84ba9029111d21e43c172c48c419f
cborg2323 <clobnjuh@gmail.com>
2023-08-31 12:56:04 -0300

    Post properties from static array

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,38 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayke Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-10 20:00:00')
+  },
+];
 
-function App() {
+export function App() {
 
   return (
     <div>
@@ -16,19 +46,20 @@ function App() {
         <Sidebar />
 
         <main>
-        <Post
-          author="Clovis Nunes"
-          content="Lorem ipsum"
-        />
-        <Post
-          author="Antonio Fiori"
-          content="Outro post"
-        />
+        
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}              
+              />
+            ) 
+          })}
+        
         </main>
       </div>
 
     </div>
   )
 }
-
-export default App
